36011244a12d9f792b12c537a8216a66fc2bff12
Eirik Sletteberg <eirik.sletteberg@digitalservice.bund.de>
2026-01-23 10:20:38 +0100

    Update text on index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,24 +22,16 @@ export default function Home() {
         <KernRow>
           <KernColumn sizes={{ xs: 12, md: 10, lg: 8 }}>
             <KernHeading level={1} size="x-large">
-              Gründungsassistent für Deutschland
+              Gründen Sie Ihr Unternehmen in Deutschlend.
             </KernHeading>
 
             <KernText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </KernText>
-
-            <KernText>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </KernText>
-
-            <KernText>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+              Schritt für Schritt zur Gründung. Kombi-Antrag für Gewerbe und Steuernummer inklusive.
             </KernText>
 
             <div style={{ marginTop: "2rem" }}>
               <Link href="/application">
-                <PrimaryButton text="Zur Anwendung" onClick={() => {}} />
+                <PrimaryButton text="Jetzt starten" onClick={() => {}} />
               </Link>
             </div>
           </KernColumn>
