120263b547b2e8a80694c2c2c4ca06cfd091e3d2
cschmidl-ds <christoph.schmidl@digitalservice.bund.de>
2026-02-04 20:47:30 +0100

    Update MockSummaryText.tsx typos

--- src/components/MockSummaryText.tsx
+++ src/components/MockSummaryText.tsx
@@ -3,7 +3,7 @@ import { KernText } from "@kern-ux-annex/kern-react-kit";
 export function MockSummaryText() {
   return (
     <KernText>
-      Sie möchten ein <strong>Einzelunternehmen</strong> gründen mit der Tätigkeit <strong>Reparatur von Möbeln und Einrichtungsgegenständen</strong>. Dies ist eine <strong>Haupttätigkeit</strong>. Sie planen <strong>nicht Mitarbeiter einzustellen</strong>.
+      Sie möchten ein <strong>Einzelunternehmen</strong> gründen mit der Tätigkeit <strong>Reparatur von Möbeln und Einrichtungsgegenständen</strong>. Dies ist eine <strong>Haupttätigkeit</strong>. Sie planen <strong>keine Mitarbeitende einzustellen</strong>.
     </KernText>
   );
 }
